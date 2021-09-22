1bef5c04b176879d0ae92fa77ab67126ae46660c
MickZ <89829528+WenhanZhu@users.noreply.github.com>
2021-09-22 03:58:22 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class Main {
+  public static void main(String[] args) {
+    Scanner s = new Scanner(System.in);
+    System.out.println("Input a number");
+    int n;
+     n=s.nextInt();
+     System.out.println(n +"\n" +n +n +"\n"+n +n +n +"\n"+n +n +n +n +"\n"+n +n +n +n +n+"\n"+n +n +n +n+"\n"+n +n +n +"\n"+n +n +"\n"+n);
+}
+}
